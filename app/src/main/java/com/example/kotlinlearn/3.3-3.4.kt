e44095aacc8475958aa16ed8e01f9f20ea4fe87a
lee <603820467@qq.com>
2020-11-22 23:03:55 +0800

    完成3.3-3.4节内容练习

--- app/src/main/java/com/example/kotlinlearn/3.3-3.4.kt
+++ app/src/main/java/com/example/kotlinlearn/3.3-3.4.kt
@@ -42,8 +42,40 @@ fun Collection<Int>.joinToString(
 val String.lastChar: Char
     get() = get(this.length - 1)
 
+//可变参数关键字
+fun tests(vararg param:String):String{
+    for (str in param){
+        println(str)
+    }
+    return ""
+}
+
+//使用infix关键字声明中缀调用
+infix fun String.to(other:Any) = Pair(this,other)
+
+//中缀调用
+fun zhongzuiInvoke(){
+    val hashMap = hashMapOf(1 to "one",2 to "Two",3 to "Three")
+    for ((index,element) in hashMap){
+        println("index=$index , element = $element")
+    }
+    val pairVal:Pair<String,Any> = "" to 1
+}
+
+//"解构声明"
+//val (index,element) = 1 to "One"
+
 fun main(args: Array<String>) {
-    println("Test".lastChar())
-    val listArray = listOf(1, 2.1, 3.5)
-    println(listArray.joinToString(separator = ":", prefix = "[", postfix = "]"))
+//    println("Test".lastChar())
+//    val listArray = listOf(1, 2.1, 3.5)
+//    println(listArray.joinToString(separator = ":", prefix = "[", postfix = "]"))
+//    val array = arrayOf(1,2,3)
+    //将数组传递给可变参数
+    //需要使用展开运算符*
+//    val list = listOf("args: ",*array)
+//    list.withIndex()
+//    println(list)
+//    tests("hahahahah","hahahaha","xixixixi")
+    //测试中缀调用
+    zhongzuiInvoke()
 }
